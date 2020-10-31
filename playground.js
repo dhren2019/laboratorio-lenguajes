39879ab868124e58391fa13905856fa15ac6b0b3
[Rodrigo] <[roglobar10@gmail.com]>
2020-10-31 12:47:46 +0100

    First commit

---
+++ playground.js
@@ -0,0 +1,3 @@
+// Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
+
+const head = (/* array */) => {}; // Implementation here.
