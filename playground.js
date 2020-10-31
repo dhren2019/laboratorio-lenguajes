61e82eecf3a76981d3ee5d186839e61983a07505
[Rodrigo] <[roglobar10@gmail.com]>
2020-10-31 13:07:21 +0100

    First commit

--- playground.js
+++ playground.js
@@ -1,3 +1,16 @@
 // Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
 
 const head = (/* array */) => {}; // Implementation here.
+
+// Implementa una función tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
+
+const tail = (/* array */) => {}; // Implementation here.
+
+// Implementa una función init (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. Utiliza los métodos que ofrece Array.prototype.
+
+const init = (/* array */) => {}; // Implementation here.
+
+// Last
+// Implementa una función last (inmutable), tal que, dado un array como entrada devuelva el último elemento.
+
+const last = (/* array */) => {}; // Implementation here.
