345ed0dba9a60adcc4a417f05ea2e1f6f647660b
[Rodrigo] <[roglobar10@gmail.com]>
2020-11-03 20:51:10 +0100

    cambio

--- playground.js
+++ playground.js
@@ -1,12 +1,14 @@
 // Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
 
-const head = ([ first, ...others]) => first; // Implementation here.
+const head = ([ first, ...others]) => first; 
 console.log(head.first)
 // Implementa una función tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
 
-const tail = ([first, ...others]) => others; // Implementation here.
+const tail = ([first, ...others]) => others; 
 
-// Implementa una función init (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. Utiliza los métodos que ofrece Array.prototype.
+// Implementa una función init (inmutable), tal que, 
+// dado un array como entrada devuelva todos los elementos menos el último.
+//  Utiliza los métodos que ofrece Array.prototype.
 
 const init = (arr) => {
     [element, ...others] = arr.reverse();
@@ -15,7 +17,11 @@ const init = (arr) => {
 console.log(init)
 
 // Last
-// Implementa una función last (inmutable), tal que, dado un array como entrada devuelva el último elemento.
+// Implementa una función last (inmutable),
+//  tal que, dado un array como entrada devuelva el último elemento.
 
-const last = () => {}; // Implementation here.
-
+const last = (arr) => {
+    [element, ...others] = arr.reverse();
+    return element;
+}; 
+console.log(last)
