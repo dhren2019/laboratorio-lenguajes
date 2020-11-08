cfd713268ed52d17199da09e90eaf19f6968969c
[Rodrigo] <[roglobar10@gmail.com]>
2020-11-08 21:00:30 +0100

    Cambio

--- arrayops.js
+++ arrayops.js
@@ -1,9 +1,14 @@
+
+// Array musicos
+const musicos = ['Michael Jackson', 'Lost Frecuencies', 'Linkin Park', 'Frank Sinatra'];
 // Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
 
 const head = ([ first, ...others]) => first; 
+console.log(head(musicos))
 // Implementa una función tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
 
 const tail = ([first, ...others]) => others; 
+console.log(tail(musicos))
 
 // Implementa una función init (inmutable), tal que, 
 // dado un array como entrada devuelva todos los elementos menos el último.
